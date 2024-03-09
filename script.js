0e510dbf2025fc941948ac05bf0e3706c066fc1a
Reuben Germain <reuben.germain@soprasteria.com>
2024-03-09 16:56:46 +0000

    added object property key pair value

--- script.js
+++ script.js
@@ -19,7 +19,11 @@ const monsterName = document.querySelector("#monsterName");
 
 const monsterHealthText = document.querySelector("#monsterHealth");
 
-const locations = [{}];
+const locations = [
+    {
+        name: "town square"
+    }
+];
 
 // initialize buttons
 button1.onclick = goStore;
