3c0b1a0df96ab3a7b3a2264c4d2aed23e8d65a91
Reuben Germain <reuben.germain@soprasteria.com>
2024-03-18 17:18:15 +0000

    set the gold & heath variables to display the values on the page

--- script.js
+++ script.js
@@ -76,6 +76,8 @@ function fightDragon() {
 function buyHealth() {
     gold -= 10;
     health += 10;
+    healthText.innerText = health;
+    goldText.innerText = gold;
 }
 
 function buyWeapon() {
