3a662fe7b76f9652bc5851f829eb7e7063632a10
Reuben Germain <reuben.germain@soprasteria.com>
2024-03-18 16:38:14 +0000

    added a third object to the locations array

--- script.js
+++ script.js
@@ -32,6 +32,13 @@ const locations = [
         'button text': ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
         "button functions": [buyHealth, buyWeapon, goTown],
         text: "You enter the store."
+    },
+
+    {
+        name: "cave",
+        'button text': ["Fight slime", "Fight fanged beast", "Go to town square"],
+        "button functions": [fightSlime, fightBeast, goTown],
+        text: "You enter the cave. You see some monsters."
     }
 ];
 
@@ -79,5 +86,5 @@ function fightSlime() {
 }
 
 function fightBeast() {
-    
+
 }
