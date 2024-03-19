2f4d3998d103b8cc939f9bab18fd78ea2071a78f
Reuben Germain <reuben.germain@soprasteria.com>
2024-03-19 16:02:48 +0000

    initialised new weapon

--- script.js
+++ script.js
@@ -108,6 +108,7 @@ function buyWeapon() {
         gold -= 30
         currentWeapon++;
         goldText.innerText = gold;
+        let newWeapon = weapons;
         text.innerText = "You now have a new weapon.";
     }
 }
